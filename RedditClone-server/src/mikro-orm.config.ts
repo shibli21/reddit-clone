52ce44afb9fcdb2fe402ce42e2be8d65191e989f
shibli21 <syedshiblimahmud@gmail.com>
2020-08-28 14:20:13 +0600

    added migrations

--- RedditClone-server/src/mikro-orm.config.ts
+++ RedditClone-server/src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import path from "path";
 
 export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+  },
   entities: [Post],
   dbName: "redditClone",
   type: "postgresql",
