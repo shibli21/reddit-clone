ee10bb10bb3155dab96b86bbad5fe609cfc3a043
shibli21 <syedshiblimahmud@gmail.com>
2020-08-28 19:21:41 +0600

    ApolloServer added with basic resolver

--- RedditClone-server/src/index.ts
+++ RedditClone-server/src/index.ts
@@ -1,21 +1,31 @@
+import { HelloResolver } from "./resolvers/hello";
 // import { Post } from "./entities/Post";
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
   const app = express();
-
-  app.get("/", (_, res) => {
-    res.send("HEllo world!");
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   });
 
+  apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
-    console.log("Server Started on localhost:4000");
+    console.log(
+      "Server Started on http://localhost:4000  && http://localhost:4000/graphql"
+    );
   });
 };
 
