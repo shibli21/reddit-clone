2c2bdd0e6c6c14b311b1b8c5c0cad7c63f8f0cf0
shibli21 <syedshiblimahmud@gmail.com>
2020-08-29 20:45:48 +0600

    register page

--- redditclone-client/src/pages/register.tsx
+++ redditclone-client/src/pages/register.tsx
@@ -5,8 +5,11 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Box,
+  Button,
 } from "@chakra-ui/core";
 import Wrapper from "../components/Wrapper";
+import InputField from "../components/InputField";
 
 interface Props {}
 
@@ -19,18 +22,28 @@ const Register = (props: Props) => {
           console.log(values);
         }}
       >
-        {({ values, handleChange }) => (
+        {({ isSubmitting }) => (
           <Form>
-            <FormControl>
-              <FormLabel htmlFor="username">User name</FormLabel>
-              <Input
-                value={values.username}
-                onChange={handleChange}
-                id="username"
-                placeholder="username"
+            <InputField
+              name="username"
+              placeholder="Username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="Password"
+                label="Password"
               />
-              {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-            </FormControl>
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variantColor="teal"
+            >
+              Register
+            </Button>
           </Form>
         )}
       </Formik>
