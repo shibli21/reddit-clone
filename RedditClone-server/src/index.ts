2a0b1faf5197c283206310e4ed98f1f0e88043aa
shibli21 <syedshiblimahmud@gmail.com>
2020-08-29 12:15:06 +0600

    setup redis

--- RedditClone-server/src/index.ts
+++ RedditClone-server/src/index.ts
@@ -1,3 +1,4 @@
+import { MyContext } from "src/types";
 import { UserResolver } from "./resolvers/user";
 import { HelloResolver } from "./resolvers/hello";
 import "reflect-metadata";
@@ -8,18 +9,43 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // *10 years
+        httpOnly: true,
+        sameSite: "lax", //csrf
+        secure: __prod__, //cookie only work is https
+      },
+      secret: "keyboard cat", // ! have to make this environment variable
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
