09071f1e6dc8a585934fab1077d3c117436b9f01
shibli21 <syedshiblimahmud@gmail.com>
2020-08-31 21:10:20 +0600

    sample posts fetch  & ssr setup

--- redditclone-client/src/pages/index.tsx
+++ redditclone-client/src/pages/index.tsx
@@ -1,11 +1,22 @@
 import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <NavBar />;
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      <div>Hello there!</div>
+      <br />
+      {!data ? (
+        <h1>Loading ..</h1>
+      ) : (
+        data.posts.map((post) => <h1 key={post.id}> {post.title}</h1>)
+      )}
+    </>
+  );
+};
 
 export default withUrqlClient(createUrqlClient)(Index);
