589ee610b1aaf9e3bb43caea22eb96b6f328802e
shibli21 <syedshiblimahmud@gmail.com>
2020-09-02 17:07:13 +0600

    forgot password mutation & pretty much authentication part done

--- redditclone-client/src/pages/forgot-password.tsx
+++ redditclone-client/src/pages/forgot-password.tsx
@@ -1,40 +1,44 @@
-import React from "react";
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
-import Wrapper from "../components/Wrapper";
-import { Formik, Form } from "formik";
-import login from "./login";
-import { toErrorMap } from "../utils/toErrorMap";
-import InputField from "../components/InputField";
 import { Box, Button } from "@chakra-ui/core";
+import { Form, Formik } from "formik";
+import { withUrqlClient } from "next-urql";
+import React, { useState } from "react";
+import InputField from "../components/InputField";
+import Wrapper from "../components/Wrapper";
+import { useForgotPasswordMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 const ForgotPassword = ({}) => {
+  const [complete, setComplete] = useState(false);
+  const [, forgotPassword] = useForgotPasswordMutation();
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ usernameOrEmail: "", password: "" }}
-        onSubmit={async (values, { setErrors }) => {
-          // const response = await login(values);
-          // if (response.data?.login.errors) {
-          //   setErrors(toErrorMap(response.data.login.errors));
-          // } else if (response.data?.login.user) {
-          //   router.push("/");
-          // }
+        initialValues={{ email: "" }}
+        onSubmit={async (values) => {
+          await forgotPassword(values);
+          setComplete(true);
         }}
       >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField name="email" placeholder="email" label="Email" />
-            <Button
-              mt={4}
-              type="submit"
-              isLoading={isSubmitting}
-              variantColor="blue"
-            >
-              Forgot Password
-            </Button>
-          </Form>
-        )}
+        {({ isSubmitting }) =>
+          complete ? (
+            <Box>
+              if an account with that username exists, we sent you an email
+            </Box>
+          ) : (
+            <Form>
+              <InputField name="email" placeholder="email" label="Email" />
+              <Button
+                mt={4}
+                type="submit"
+                isLoading={isSubmitting}
+                variantColor="blue"
+              >
+                Forgot Password
+              </Button>
+              // forgot password mutation & pretty much authentication part done
+            </Form>
+          )
+        }
       </Formik>
     </Wrapper>
   );
