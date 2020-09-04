ea02816e8abfc3940cd7ed5948a1e3b165c1b0cc
shibli21 <syedshiblimahmud@gmail.com>
2020-09-04 21:31:54 +0600

    new post mutations generated

--- redditclone-client/src/generated/graphql.tsx
+++ redditclone-client/src/generated/graphql.tsx
@@ -40,6 +40,7 @@ export type Post = {
   creatorId: Scalars['Float'];
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
+  textSnippet: Scalars['String'];
 };
 
 export type User = {
@@ -238,7 +239,7 @@ export type PostsQuery = (
   { __typename?: 'Query' }
   & { posts: Array<(
     { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'title' | 'text' | 'points' | 'creatorId' | 'createdAt' | 'updatedAt'>
+    & Pick<Post, 'id' | 'title' | 'textSnippet' | 'points' | 'createdAt' | 'updatedAt'>
   )> }
 );
 
@@ -349,9 +350,8 @@ export const PostsDocument = gql`
   posts(limit: $limit, cursor: $cursor) {
     id
     title
-    text
+    textSnippet
     points
-    creatorId
     createdAt
     updatedAt
   }
