aed0eb6ecbe1697ac8c47dcb7701bfcc29b46e5f
shibli21 <syedshiblimahmud@gmail.com>
2020-09-05 20:26:20 +0600

    pagination added!!!!!!!!!!!!!!!!!!!!!!!!!! :'(

--- redditclone-client/src/pages/index.tsx
+++ redditclone-client/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
+import { Box, Flex, Heading, Link, Stack, Text, Button } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
 import Layout from "../components/Layout";
@@ -6,26 +6,29 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
-      limit: 50,
+      limit: 10,
     },
   });
+  if (!data && !fetching) {
+    return <div>you got query failed for some reason</div>;
+  }
   return (
     <Layout>
       <Flex alignItems="center">
-        <Heading color="teal">S:Redit</Heading>
+        <Heading color="teal">S REDIT</Heading>
         <NextLink href="/create-post">
           <Link ml="auto">create post</Link>
         </NextLink>
       </Flex>
 
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <h1>Loading ..</h1>
       ) : (
         <Stack spacing={8}>
-          {data.posts.map((post) => (
+          {data!.posts.map((post) => (
             <Box key={post.id} p={5} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{post.title}</Heading>
               <Text mt={4}>{post.textSnippet}</Text>
@@ -33,6 +36,13 @@ const Index = () => {
           ))}
         </Stack>
       )}
+      {data ? (
+        <Flex>
+          <Button m="auto" my={4}>
+            Load more
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
