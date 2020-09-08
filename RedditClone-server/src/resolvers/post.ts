9c339a19bd2b461083b7a18106b394c4b8a9435c
shibli21 <syedshiblimahmud@gmail.com>
2020-09-08 19:47:31 +0600

    delete post

--- RedditClone-server/src/resolvers/post.ts
+++ RedditClone-server/src/resolvers/post.ts
@@ -238,9 +238,14 @@ export class PostResolver {
     }
     return post;
   }
+
   @Mutation(() => Boolean)
-  async deletePost(@Arg("id") id: number): Promise<boolean> {
-    await Post.delete(id);
+  @UseMiddleware(isAuth)
+  async deletePost(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { req }: MyContext
+  ): Promise<boolean> {
+    await Post.delete({ id, creatorId: req.session!.userId });
     return true;
   }
 }
